7e22d795c8f23b6a180e5b7734e79eacd4a3f64c
sberger94 <sberger94@gmail.com>
2022-07-22 15:43:16 -0700

    Added animation to title

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,27 @@
-import React from "react";
-import "./index.scss";
+import React, { useState, useEffect } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 import Loader from "react-loaders";
+import "./index.scss";
 
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, []);
+
   return (
   <>
     <div className="container portfolio-page">
-      <h1 className="page-title">Portfolio</h1>
+      <h1 className="page-title">
+        <AnimatedLetters 
+          letterClass={letterClass} 
+          strArray={['P','o','r','t','f','o','l','i','o']}
+          idx={15}
+        />
+      </h1>
     </div>
     <Loader type="ball-scale-ripple" />
   </>
